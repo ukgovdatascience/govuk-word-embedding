42ff034b6452519fe0c247600dfb438dc1bd9727
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-10-27 16:28:36 +0100

    Use env vars in run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 ENV=./.env
-DATA=$DATA_DIR
-OUTPUT=$OUT_DIR
 
 docker run -i --rm \
     --env-file $ENV \
-    -v $DATA:/mnt/DATA \
-    -v $OUTPUT:/mnt/output \
+    -v $DATA_DIR:/mnt/DATA \
+    -v $OUT_DIR:/mnt/output \
     ukgovdatascience/govuk-word-embedding:latest python create-vocabulary.py
 
 docker run -i $ENV \
     --env-file ./.env \
-    -v $DATA:/mnt/DATA \
-    -v $OUTPUT:/mnt/output \
+    -v $DATA_DIR:/mnt/DATA \
+    -v $OUT_DIR:/mnt/output \
     ukgovdatascience/govuk-word-embedding:latest python build-word-embedding.py
 
 docker run -i --rm \
     --env-file $ENV \
-    -v $DATA:/mnt/DATA \
-    -v $OUTPUT:/mnt/output \
+    -v $DATA_DIR:/mnt/DATA \
+    -v $OUT_DIR:/mnt/output \
     ukgovdatascience/govuk-word-embedding:latest python tsne-plot.py
 
