099f393f47c62f779704234b3dc36317cf1c8846
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-10-27 12:21:04 +0100

    Update volume mount paths

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker run -i --rm \
     --env-file ./.env \
-    -v /home/ubuntu/DATA:/mnt/DATA \
-    -v /home/ubuntu/output:/mnt/output \
+    -v /data:/mnt/DATA \
+    -v /data/output:/mnt/output \
     ukgovdatascience/govuk-word-embedding:latest python build-word-embedding.py
 
